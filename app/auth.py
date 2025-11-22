0669f25dbb8c7fee8508831359b2a1379eb35d92
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-11-22 17:55:47 +0100

    update

--- app/auth.py
+++ app/auth.py
@@ -1,18 +1,18 @@
 from fastapi import HTTPException
 from app.config import SECRET_KEY, ALGORITHM 
 from jose import jwt
-# from datetime import datetime, timedelta
-# import sys, os
-
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# from datetime import datetime, timedelta, timezone
 from app.config import SECRET_KEY, ALGORITHM 
-from jose import jwt
+
 
 
 
 
 def create_token(username):
-    payload = {}
+    payload = {
+        # "sub": username,
+        # "exp": datetime.now(timezone.utc) + timedelta(hours=1)
+    }
 
     token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
     return token
@@ -22,4 +22,4 @@ def verify_token(token):
        token_new = jwt.decode(token, SECRET_KEY, algorithms= ALGORITHM)
     except :
         raise HTTPException(status_code=401, detail="token invalide")
-        return data
+        return token_new
