8e84d89981d4a4cebb823d912b1087593914e2e4
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-11-22 17:54:27 +0100

    update

--- app/model.py
+++ app/model.py
@@ -1,14 +1,5 @@
-from sqlalchemy import String, Integer, Column
-# from app.database import Base
 from pydantic import BaseModel
 
-
-
-# class User(Base):
-#     __tablename__ = "users"
-#     id = Column(Integer, primary_key=True)
-#     username = Column(String, unique=True, nullable=False)
-#     password = Column(String, nullable=False)
 
 
 class UserCreate(BaseModel):
