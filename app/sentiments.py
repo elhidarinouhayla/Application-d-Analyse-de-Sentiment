c36a869f463d3ad935e27c243dc27df593841077
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-11-23 15:49:54 +0100

    update

--- app/sentiments.py
+++ app/sentiments.py
@@ -8,31 +8,31 @@ def get_sentiment(input_text):
         "Authorization": f"Bearer {HF_TOKEN}"
     }
 
-    def query(payload):
-        response = requests.post(API_URL, headers=headers, json=payload)
-        return response.json()
+    # def query(payload):
+    #     response = requests.post(API_URL, headers=headers, json=payload)
+    #     return response.json()
 
-    output = query({
-        "inputs": input,
-    }) 
-    return output
-
-
+    # output = query({
+    #     "inputs": input,
+    # }) 
+    # return output
 
 
 
 
-    # try:
-    #     response = requests.post(API_URL, headers=headers, json={"inputs": input_text}, timeout=10)
-    #     response.raise_for_status()
-    #     output = response.json()
+
+
+    try:
+        response = requests.post(API_URL, headers=headers, json={"inputs": input_text}, timeout=10)
+        response.raise_for_status()
+        output = response.json()
         
-    #     if isinstance(output, dict) and "error" in output:
-    #         return [{"label": "error", "score": 0}]
-    #     return output
+        if isinstance(output, dict) and "error" in output:
+            return [{"label": "error", "score": 0}]
+        return output
 
-    # except requests.RequestException:
-    #     return [{"label": "error", "score": 0}]
+    except requests.RequestException:
+        return [{"label": "error", "score": 0}]
     
 
   
